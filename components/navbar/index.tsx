95817e1082f4aa5173783f861d778c390bdf6d57
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-04-06 15:06:54 -0300

    create navbar component

---
+++ components/navbar/index.tsx
@@ -0,0 +1,9 @@
+import { NavBarContainer, Logo } from './styles'
+
+export default function NavBar() {
+    return (
+        <NavBarContainer>
+            <Logo src="logo.svg" alt="blog-logo"/>
+        </NavBarContainer>
+    )
+}
---
+++ components/navbar/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const NavBarContainer = styled.nav`
+    height: 100px;
+    width: 100%;
+    background: ${props => props.theme.primary};
+`
+export const Logo = styled.img`
+    height: inherit;
+    padding: 1rem;
+`
