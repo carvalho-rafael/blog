02ec948de9027e869c8a953f3299e7819b887397
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-05-17 23:24:28 -0300

    create pagination to posts

--- components/posts/index.tsx
+++ components/posts/index.tsx
@@ -1,12 +1,36 @@
 import { PostItem, PostsContainer } from './styles';
 import { lorem, date } from 'faker';
 import Link from 'next/link'
+import { useEffect, useRef, useState } from 'react';
 
-export default function Posts({posts}) {
+type Post = {
+    id: string
+    title: string
+    cover: string
+    description: string
+    body: string
+    createdAt: string
+}
 
+type PostsProps = {
+    posts: Post[]
+}
+
+const PAGE_POSTS = 5;
+
+export default function Posts({ posts }: PostsProps) {
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [currentPosts, setCurrentPosts] = useState<Post[]>(posts.slice(0, PAGE_POSTS))
+    const pages = useRef(Math.ceil(posts.length / PAGE_POSTS))
+
+    useEffect(() => {
+        setCurrentPosts(posts.slice((currentPage-1) * PAGE_POSTS, (currentPage-1) * PAGE_POSTS + PAGE_POSTS))
+    }, [currentPage])
+    
     return (
         <PostsContainer>
-            {posts.map(post => (
+            {currentPosts.map(post => (
                 <PostItem key={post.id}>
                     <img src={post.cover} alt="" />
                     <div>
@@ -18,6 +42,12 @@ export default function Posts({posts}) {
                     </div>
                 </PostItem>
             ))}
+            <h1>{currentPage}</h1>
+            <div>
+                {[...Array(pages.current)].map((page, index) => (
+                    <a onClick={() => setCurrentPage(index + 1)}>{index + 1}</a>
+                ))}
+            </div>
         </PostsContainer>
     )
 }
